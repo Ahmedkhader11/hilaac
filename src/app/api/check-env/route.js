20c5dd16683f22a46fbb82030bde334f42d737e5
Ahmedkhader11 <ahmetkhater43@gmail.com>
2025-05-19 13:21:06 +0300

    deleted some unwanted files and folders

--- src/app/api/check-env/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-// api/check-env/route.js
-export async function GET() {
-  console.log("Raw ENV:", process.env.MONGODB_URL);
-  return NextResponse.json({
-    dbUrl: process.env.MONGODB_URL,
-  });
-}
